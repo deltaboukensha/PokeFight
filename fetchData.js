f4fe944837c826f740893291707d95c546f81a34
Daniel Zheng <daniel.zheng@barnebys.com>
2020-04-17 16:19:53 +0900

    update fetch data

--- fetchData.js
+++ fetchData.js
@@ -16,10 +16,12 @@ const runAsync = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    for(const {url, tag} of sources){
-        await page.goto(url);
+    for(const source of sources){
+        await page.goto(source.url);
         const content = await page.content();
-        fs.writeFileSync(`./data/${tag}.txt`, content, "utf-8");
+        const url = new URL(source.url);
+        fs.mkdirSync(`./data/${url.hostname}/${url.pathname}/`, { recursive: true });
+        fs.writeFileSync(`./data/${url.hostname}/${url.pathname}/data.txt`, content, "utf-8");
     }
 
     await browser.close();
