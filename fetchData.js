a82b4e092e0eb1ec7d3275510ebe9707821bed73
Daniel Zheng <daniel.zheng@barnebys.com>
2020-04-17 13:51:41 +0900

    update pokemon types

--- fetchData.js
+++ fetchData.js
@@ -2,9 +2,13 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 const sources = [
+    // {
+    //     url: "https://gamepress.gg/pokemongo/pokemon-go-type-chart",
+    //     tag: "pokemon-go-type-chart"
+    // },
     {
-        url: "https://gamepress.gg/pokemongo/pokemon-go-type-chart",
-        tag: "pokemon-go-type-chart"
+        url: "https://www.eurogamer.net/articles/2018-12-21-pokemon-go-type-chart-effectiveness-weaknesses",
+        tag: "pokemon-go-type-table"
     }
 ];
 
--- updateTypes.js
+++ updateTypes.js
@@ -2,27 +2,40 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const runAsync = async () => {
-    var pageData = fs.readFileSync("./data/pokemon-go-type-chart.txt", "utf-8")
-    const browser = await puppeteer.launch();
+    var pageData = fs.readFileSync("./data/pokemon-go-type-table.txt", "utf-8")
+    const browser = await puppeteer.launch({
+        headless: true
+    });
     const page = await browser.newPage();
     await page.setContent(pageData);
-    const typeNames = await page.$$eval("sapn", result => {
-        return result.map(i => i.innerText);
+
+    const evalResult = await page.evaluate(() => {
+        const data = [];
+        document.querySelectorAll("#page-wrapper > main > div.document > div.main > article > div.body > section:nth-child(3) > div > table > tbody > tr > td").forEach(i => data.push(i.innerText))
+        const list = [];
+
+        for (let i = 0; i < data.length; i += 5) {
+            const name = data[i + 0];
+            const strongAgainst = data[i + 1].split(",").map(i => i.trim()).filter(i => i);
+            const weakAgainst = data[i + 2].split(",").map(i => i.trim()).filter(i => i);
+            const resistantTo = data[i + 3].split(",").map(i => i.trim()).filter(i => i);
+            const vulnerableTo = data[i + 4].split(",").map(i => i.trim()).filter(i => i);
+            list.push({
+                name,
+                strongAgainst,
+                weakAgainst,
+                resistantTo,
+                vulnerableTo
+            });
+        }
+
+        return list;
     });
 
-    const pokemonTypes = typeNames.map(typeName => {
-        return {
-            "name": typeName
-        };
-    });
-
-    console.log(pokemonTypes)
-
-    fs.writeFileSync(`./data/pokemonTypes.js`, JSON.stringify(pokemonTypes, null, 4), "utf-8");
-
+    fs.writeFileSync(`./data/pokemonTypes.js`, JSON.stringify(evalResult, null, 4), "utf-8");
     await browser.close();
 };
 
 runAsync()
-.catch(console.error)
-.then(() => process.exit(0))
+    .catch(console.error)
+    .then(() => process.exit(0))
